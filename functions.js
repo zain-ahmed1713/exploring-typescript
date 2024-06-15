bd5eb7728191b33471455b3fa716afdc598e97d4
Zain Ahmed <zainahmed1713@gmail.com>
2024-06-16 03:23:43 +0500

    Functions, parameter types and return types

---
+++ functions.js
@@ -0,0 +1,28 @@
+// Type inference doesn't help in function parameters so we have to explicitly define types
+var divideByTwo = function (num) {
+    return num / 2;
+};
+var result = divideByTwo(8);
+var signup = function (name, age, email, password) {
+    return "User Signed Up";
+};
+var result1 = signup("Zain", 23, "zain@zain.com", "123abc");
+// We can also define return types
+var addThree = function (val) {
+    return val + 3;
+};
+// Void type
+// Used when a function doesn't return anything
+var sayHello = function (name) {
+    console.log("Hello, ".concat(name));
+};
+// never type
+// Used when a function is used to throw some errors and doesn't return a value
+var throwError = function (errMsg) {
+    throw new Error(errMsg);
+};
+// Array map method
+var languages = ["JS", "CPP", "Python"];
+languages.map(function (val, index) {
+    return "Value: ".concat(val, ", Index: ").concat(index);
+});
---
+++ functions.ts
@@ -0,0 +1,35 @@
+// Type inference doesn't help in function parameters so we have to explicitly define types
+const divideByTwo = (num: number) => {
+    return num / 2;
+}
+
+const result = divideByTwo(8);
+
+const signup = (name: string, age: number, email: string, password: string) => {
+    return "User Signed Up"
+}
+
+const result1 = signup("Zain", 23, "zain@zain.com", "123abc");
+
+// We can also define return types
+const addThree = (val: number): number => {
+    return val + 3;
+}
+
+// Void type
+// Used when a function doesn't return anything
+const sayHello = (name: string): void => {
+    console.log(`Hello, ${name}`)
+}
+
+// never type
+// Used when a function is used to throw some errors and doesn't return a value
+const throwError = (errMsg: string): never => {
+    throw new Error(errMsg)
+}
+
+// Array map method
+const languages = ["JS", "CPP", "Python"]
+languages.map((val, index): string => {
+    return `Value: ${val}, Index: ${index}`;
+})
